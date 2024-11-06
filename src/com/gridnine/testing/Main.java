e06b32e5b9e412edae13d11817fd25252411be2c
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-11-06 23:48:48 +0300

    Flight Filter - начальный коммит

---
+++ src/com/gridnine/testing/Main.java
@@ -0,0 +1,11 @@
+package com.gridnine.testing;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Flight Filter invoke...");
+        System.out.println();
+
+    }
+
+}
